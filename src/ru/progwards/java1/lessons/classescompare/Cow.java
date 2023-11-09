3a0bc1062ade3b924b7cbdb7ec167b09ff8760d1
brightside22 <brightside227@gmail.com>
2023-11-09 15:05:18 +0300

    HomeWork 18 test

--- src/ru/progwards/java1/lessons/classescompare/Cow.java
+++ src/ru/progwards/java1/lessons/classescompare/Cow.java
@@ -1,27 +1,2 @@
 package ru.progwards.java1.lessons.classescompare;
 
-public class Cow extends Animal {
-    public Cow(String name) {
-        super(name);
-
-    }
-
-    public String kind() {
-        return "корова";
-    }
-
-    public String say() {
-        return "мууууууу";
-    }
-
-
-    public FoodKind getFoodKind() {
-
-        return FoodKind.HAY;
-    }
-
-    public double getFoodCoeff() {
-        return 0.05;
-    }
-
-}
